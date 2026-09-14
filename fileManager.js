501131c3a69794911162d88983dd0300cb6e41fd
anshsehrawat2707-create <anshsehrawat2707@gmail.com>
2026-09-14 21:59:10 +0530

    Smart Utility Toolkit - Assignment 1

---
+++ fileManager.js
@@ -0,0 +1,41 @@
+﻿const fs = require("fs");
+
+const fileName = "test.txt";
+
+console.log("Creating File...");
+fs.writeFile(fileName, "Hello Node.js", function (err) {
+  if (err) {
+    console.log("Error creating file");
+    return;
+  }
+  console.log("File Created");
+
+  fs.readFile(fileName, "utf8", function (err, data) {
+    if (err) {
+      console.log("Error reading file");
+      return;
+    }
+    console.log("Reading File");
+    console.log(data);
+
+    fs.appendFile(fileName, "\nLearning FS Module", function (err) {
+      if (err) {
+        console.log("Error updating file");
+        return;
+      }
+      console.log("File Updated");
+
+      fs.readFile(fileName, "utf8", function (err, data) {
+        console.log(data);
+
+        fs.unlink(fileName, function (err) {
+          if (err) {
+            console.log("Error deleting file");
+            return;
+          }
+          console.log("File Deleted");
+        });
+      });
+    });
+  });
+});
---
+++ modules/logger.js
@@ -0,0 +1,6 @@
+﻿function logMessage(message) {
+  const time = new Date().toLocaleTimeString();
+  console.log("[" + time + "] " + message);
+}
+
+module.exports = logMessage;
